29ad1557e70ca0ccfea933cb9187edc95f60a4da
JacksonJ01 <jacksonj@hartwick.edu>
2019-10-31 08:48:04 -0400

    This is what i was able to add last night i fell asleep

--- GroupGame.py
+++ GroupGame.py
@@ -4,17 +4,36 @@
 # Candy Land
 
 from random import *
-
 location = []
+
 
 def dice():
     roll_dice = randint(1, 8)
     print(roll_dice)
 
 
+def candy_land():
+    print()
 
 
+def play():
+    play1 = input(">>>").title()
+    if play1 == "Yes" or "Y":
+        print("Alright, lettuce get this show on the road!")
+    elif play1 == "No" or "N":
+        print("Lml, noob! Too bad for you! XD")
+    else:
+        print("Yes or No answers please.")
+        play()
 
 
-dice()  # Test call
-
+print("Hello user, what is your name?")
+name = input(">>>").title()
+print(f"Ahh, nice to meet you {name}.")
+print("My name is...")  # Make up a name M.
+print("Would you like gto play a game? *Yes or No*")
+play()
+print(f"""This game is called Candy Land.
+Let me explain the rules to you, {name}.
+""")
+candy_land()
