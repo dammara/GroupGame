1669c44e33c2afd5811d2349cf73a4bd2301ac50
dammara <h3246531>
2019-10-31 09:22:26 -0400

    10/31/19 9:22AM - Set up Splash Screen and "Rules" function

--- GroupGame.py
+++ GroupGame.py
@@ -13,27 +13,38 @@ def dice():
 
 
 def candy_land():
-    print()
+    print("")
 
+
+def rules():
+    print("""1. You will roll a dice to move about the board.
+    2. You must get to spot 20 to win. If you land on 0, you lose!
+    3. If you roll a 1 or 3, you move back 1 step.
+    4. If you roll a 5, you move back 2 steps.
+    5. If you roll a 2 or 4, you move forward 1 step.
+    6. If you roll a 6 or 8, you move forward 2 steps.
+    7. If you roll a lucky 7, you get to roll again.""")
 
 def play():
     play1 = input(">>>").title()
-    if play1 == "Yes" or "Y":
+    if play1 == "Yes" or "Y" or "y" or "yes":
         print("Alright, lettuce get this show on the road!")
-    elif play1 == "No" or "N":
+    elif play1 == "No" or "N" or "no" or "n":
         print("Lml, noob! Too bad for you! XD")
     else:
         print("Yes or No answers please.")
         play()
 
 
+# Splash Screen
 print("Hello user, what is your name?")
 name = input(">>>").title()
 print(f"Ahh, nice to meet you {name}.")
 print("My name is...")  # Make up a name M.
-print("Would you like gto play a game? *Yes or No*")
+print("Would you like to play a game? *Yes or No*")
 play()
 print(f"""This game is called Candy Land.
-Let me explain the rules to you, {name}.
 """)
+print("Let me explain the rules to you, {name}.")
+rules()
 candy_land()
